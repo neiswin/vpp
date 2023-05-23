b327a53de7434fc6284eda693dff3546b5f7bff7
neiswin <nikolaswind@gmail.com>
2023-05-23 11:05:50 +0300

    opc-connect

--- opctest.rb
+++ opctest.rb
@@ -3,11 +3,11 @@ require 'opcua_client'
 client = OPCUAClient::Client.new
 begin
   puts "start connect"
-  client.connect("opc.tcp://127.0.0.1:55555")
+  client.connect("opc.tcp://192.168.0.22:4840")
   # write to ns=2;s=1
   puts "connect done"
 
-  puts client.read_float(1, "OPCDA.client.Sinusoid.PsFloat1")
+  puts client.write_boolean(4 , "|var|PLC210 (Arm32bit + Linux ).Application.PLC_PRG.xB", true)
 
 ensure
   client.disconnect
